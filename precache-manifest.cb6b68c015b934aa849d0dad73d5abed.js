67a0089acac9d74ad732d510798180b61745f783
NiiXu <chenhengxv@outlook.com>
2022-03-08 14:58:54 +0800

    deploy

---
+++ precache-manifest.cb6b68c015b934aa849d0dad73d5abed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eadad1de69f20fddb72",
+    "url": "/morney-web/css/app.346b358b.css"
+  },
+  {
+    "revision": "8c0736627ad7cfdd4c23502f77d8eb5a",
+    "url": "/morney-web/index.html"
+  },
+  {
+    "revision": "2eadad1de69f20fddb72",
+    "url": "/morney-web/js/app.de59b49c.js"
+  },
+  {
+    "revision": "af2341a8980a6a8da17f",
+    "url": "/morney-web/js/chunk-vendors.e06a0b8f.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-web/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-web/robots.txt"
+  }
+]);
